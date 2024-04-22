925fd0e9ab4fe4a4125f77d140ad515e0eaf91fb
Timotius Nugroho <timotius.nugroho@aruna.id>
2024-04-22 21:11:03 +0700

    custom env var

--- src/main/java/learnjava/restfullapi/controller/UserController.java
+++ src/main/java/learnjava/restfullapi/controller/UserController.java
@@ -1,6 +1,7 @@
 package learnjava.restfullapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,10 +17,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Value("${spring.meta.greeting}")
+    private String greetingText;
+
     @PostMapping(path = "/users", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
         userService.register(request);
-        return WebResponse.<String>builder().data("ok").build();
+        return WebResponse.<String>builder().data("ok " + greetingText).build();
     }
 
 }
